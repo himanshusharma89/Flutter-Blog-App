89c18608eaa4e25d2da7109dc1b5601041ae9eee
Ayush Raj <beast@localhost.localdomain>
2020-10-13 17:45:10 +0530

    added screen transition animation

--- lib/routing/route_page.dart
+++ lib/routing/route_page.dart
@@ -8,26 +8,41 @@ import '../screens/home.dart';
 import '../screens/profile.dart';
 import '../screens/view_post.dart';
 import 'undefined_view.dart';
+import 'package:page_transition/page_transition.dart';
+
+/*
+*  we can use any type of screen transition.
+*   fade,
+  rightToLeft,
+  leftToRight,
+  upToDown,
+  downToUp,
+  scale,
+  rotate,
+  size,
+  rightToLeftWithFade,
+  leftToRightWithFade,
+  * */
 
 class RoutePage {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case RouteConstant.ROOT:
-        return MaterialPageRoute(builder: (_) => HomePage());
+        return PageTransition(child: HomePage(),duration: Duration(seconds: 1), type: PageTransitionType.leftToRight);
 
       case RouteConstant.ADD_POST:
-        return MaterialPageRoute(builder: (_) => AddPost());
+        return PageTransition(child: AddPost(),duration: Duration(seconds: 1), type: PageTransitionType.leftToRight);
 
       case RouteConstant.EDIT_POST:
         Post post = settings.arguments as Post;
-        return MaterialPageRoute(builder: (_) => EditPost(post));
+        return PageTransition(child: EditPost(post),duration: Duration(seconds: 1), type: PageTransitionType.leftToRightWithFade);
 
       case RouteConstant.VIEW_POST:
         Post post = settings.arguments as Post;
-        return MaterialPageRoute(builder: (_) => PostView(post));
+        return PageTransition(child: PostView(post),duration: Duration(seconds: 1), type: PageTransitionType.leftToRight);
 
       case RouteConstant.PROFILE:
-        return MaterialPageRoute(builder: (_) => Profile());
+        return PageTransition(child: Profile(),duration: Duration(seconds: 1), type: PageTransitionType.leftToRight);
 
       default:
         return MaterialPageRoute(
