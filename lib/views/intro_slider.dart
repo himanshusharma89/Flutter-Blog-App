07ab679974807fd1febec9fde09a77e3c2bc8589
Saransh <saransh0701@gmail.com>
2021-01-28 20:09:14 +0530

    Added complete intro slider

---
+++ lib/views/intro_slider.dart
@@ -0,0 +1,163 @@
+import 'package:blog_app/models/page_view.dart';
+import 'package:blog_app/views/home.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+var introSlider = [
+  PageViewModel(
+    text:
+    "Do you have ideas that you want to pen down?",
+    image: "Blog3.png",
+  ),
+  PageViewModel(
+    text: "Looking for a spot to write blogs?",
+    image: "Blog2.png",
+  ),
+  PageViewModel(
+    text: "You came to the right place!\nWrite, read and even fetch articles from internet!",
+    image: "Blog1.jpg",
+  ),
+];
+
+class IntroScreen extends StatefulWidget {
+  const IntroScreen({Key key}) : super(key: key);
+
+  @override
+  _IntroScreenState createState() => _IntroScreenState();
+}
+
+class _IntroScreenState extends State<IntroScreen> {
+  PageController _pageController;
+
+  int _currentPage = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+    _pageController.addListener(() {
+      if (_currentPage != _pageController.page.round()) {
+        setState(() {
+          _currentPage = _pageController.page.round();
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Material(
+        color: Colors.white,
+        child: Stack(
+          children: <Widget>[
+            Positioned.fill(
+              top: 40,
+              bottom: 49,
+              child: SizedBox(
+                height: 200,
+                child: PageView(
+                  pageSnapping: true,
+                  controller: _pageController,
+                  children: introSlider,
+                ),
+              ),
+            ),
+            _appBar(),
+            bottomNavigation(context)
+          ],
+        ),
+      ),
+    );
+  }
+
+  Align bottomNavigation(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Row(
+        children: [
+          const SizedBox(width: 15.0),
+          SmoothPageIndicator(
+            controller: _pageController,
+            count: 3,
+            effect: WormEffect(
+              activeDotColor: Colors.blue,
+              dotHeight: 12.0,
+              dotWidth: 12.0,
+            ),
+          ),
+          Spacer(),
+          AnimatedSwitcher(
+            duration: const Duration(milliseconds: 200),
+            child: _currentPage != 2
+                ? SizedBox(
+              width: 90,
+              height: 49,
+              child: IconButton(
+                splashColor: Colors.transparent,
+                padding: const EdgeInsets.all(0.0),
+                onPressed: () {
+                  _pageController.nextPage(
+                    duration: const Duration(milliseconds: 300),
+                    curve: Curves.linear,
+                  );
+                },
+                icon: Icon(Icons.arrow_forward_ios_rounded),
+              ),
+            )
+                : InkWell(
+              onTap: () =>
+                  Navigator.push(
+                      context, MaterialPageRoute(builder: (_) => HomePage())),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(15.0)),
+                ),
+                width: 90,
+                height: 49,
+                child: Center(
+                  child: Text(
+                    "START",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Align _appBar() {
+    return Align(
+      alignment: Alignment.topCenter,
+      child: Padding(
+        padding: const EdgeInsets.only(top: 3.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(width: 10),
+            Image.asset(
+              "assets/blog_flutter_light.png",
+              width: 100,
+              height: 100,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
